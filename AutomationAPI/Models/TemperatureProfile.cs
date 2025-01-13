19e6ad00123fd1d8d44340683db9bf7635f1c28e
ynr2321 <ynr2321@gmail.com>
2025-01-13 21:33:31 +0000

    AutomationAPI - Added TemperatureProfile model

---
+++ AutomationAPI/Models/TemperatureProfile.cs
@@ -0,0 +1,11 @@
+﻿namespace AutomationAPI.Models
+{
+    public class TemperatureProfile
+    {
+        public int ProfileId { get; set; } 
+        public DateTime CreatedDate { get; set; }
+        public string Name { get; set; }
+        public string Profile { get; set; }
+        public DateTime ModifiedDate { get; private set; }
+    }
+}
