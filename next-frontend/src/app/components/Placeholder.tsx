326332ca1c3421576d38c6fe669d399286bfe742
ynr2321 <ynr2321@gmail.com>
2025-02-02 19:58:00 +0000

    next-frontend - Updated temporary placeholder logic for testing connection / permissions with backend

--- next-frontend/src/app/components/Placeholder.tsx
+++ next-frontend/src/app/components/Placeholder.tsx
@@ -1,33 +1,41 @@
+
+
 import fetch from "node-fetch";
 import * as https from "https";
 
 export default async function Placeholder() {
-	let message = "Could not get message";
+	let message = "Could not fetch from API";
+
 	try {
-		console.log("Fetching data from endpoint");
-		// Fetching logic
-		const agent = new https.Agent({ rejectUnauthorized: false });
-		const options = {
-			method: "GET",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			agent: agent,
+		// Prep agent to accept data from .NET Backend with self-signed certificate
+		const options =
+		{
+			agent:  new https.Agent({ rejectUnauthorized: false })
 		};
-		const result = await fetch("https://localhost:32769/Placeholder", options);
 
-		console.log(`Response Status: ${result.status}`, result.statusText);
+		// Fetch data
+		console.log("Fetching data from API");
+		const result = await fetch("https://localhost:7218/api/profiles/2", options);
+		const rawResponse = await result.text();
+		console.log(`Raw Response: ${rawResponse}`);
 
+		// Validate
 		if (!result.ok) {
 			throw new Error(`Server Error: ${result.status} ${result.statusText}`);
 		}
-		const jsonResponse = await result.json();
-		message = jsonResponse.message;
-	} catch (error) {
+
+		// Set message to display
+		message = JSON.stringify(rawResponse);
+
+	} 
+	catch (error) 
+	{
 		console.error("Fetch Error:", error);
-		message = `An error occurred while fetching data ${error}`;
+		message = `An error occured: ${error}`;
 	}
 
+
+	// Return tsx
 	return (
 		<div>
 			<h1>Fetched Data:</h1>
